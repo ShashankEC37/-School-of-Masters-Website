73cff2751e34b97da96e887dae972a340cc5732d
ShashankEC37 <jsr.shashank@gmail.com>
2023-07-06 08:24:37 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
+import {FaBehance, FaDribbble} from 'react-icons/fa';
+import {IoMailOutline, IoChevronForwardCircle} from 'react-icons/io5';
+import {IconContext} from "react-icons"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header >
+                <div className="logo_wrapper">SOM</div>
+                <div className="menu_container">
+                    <span>
+                      <IconContext.Provider value={{color:"#000",size:"18px", className:"icon_container"}}>
+                      <div className='icon'><FaBehance/> </div>
+                      <div className='icon'><FaDribbble/> </div>
+                      </IconContext.Provider>
+                    </span>
+                    <span>
+                    <IconContext.Provider value={{color:"#000",size:"18px" }}>
+                      <div className='icon'><IoMailOutline/>sheetal@schoolofmasters.co.in </div>
+                      </IconContext.Provider>
+                    </span>
+                    <span className='menu'>
+                    
+                      <span></span>
+                      <span></span>
+                      <span></span>
+
+                    </span>
+                </div>
       </header>
-    </div>
+
+      <div className='constent_wrapper'>
+        <div className='left_content_wrapper'>
+          <h2>
+            <span>Navigating the Path to </span>
+            <span>Corporate Excellence</span>
+            </h2>
+            <p>
+            Unlock the potential with experts specializing in <br />corporate training, strategic recruitments, and personalized 1-to-1 training .
+            </p>
+            <div className="btn_group"></div>
+              <div className='btn btn_primary'>Lets Connect
+              <IconContext.Provider value={{color:"#14da84",size:"25px" }}>
+                     <IoChevronForwardCircle/>
+                      </IconContext.Provider>
+              </div>
+            <span></span>
+
+
+          </div>  
+         </div>
+        <div className='left_content_wrapper'>
+        </div>
+    </>
   );
 }
 
